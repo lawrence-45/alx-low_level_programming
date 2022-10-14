e21a206a9e3e5e74ade6da556d77a8dbb3dcc66d
lawrence-45 <lawrenceanekwe@gmail.com>
2022-10-13 18:01:21 -0700

    Answer task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %lu\n", sizeof(char));
+	printf("Size of an int: %lu\n", sizeof(int));
+	printf("Size of long int: %lu\n", sizeof(long int));
+	printf("Size of long long int: %lu\n", sizeof(long long int));
+	printf("Size of a float: %lu\n", sizeof(float));
+}
