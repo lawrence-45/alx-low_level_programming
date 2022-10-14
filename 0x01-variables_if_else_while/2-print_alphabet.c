564076e49e2aec2bae06c38f790bfb9e6af03df3
lawrence-45 <lawrenceanekwe@gmail.com>
2022-10-14 15:07:48 -0700

    Answer task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	for (char n = 'a' ; n <= 'z'; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
