5f9af1152602d11b5f8afc363052e5fe448f44af
lawrence-45 <lawrenceanekwe@gmail.com>
2022-10-14 12:58:08 -0700

    Modifies task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,15 +19,15 @@ int main(void)
 	printf("Last digit of %d is %d", n, lastdigi);
 	if (lastdigi > 5)
 	{
-		printf("and is greater than 5\n");
+		printf(" and is greater than 5\n");
 	}
 	if (lastdigi == 0)
 	{
-		printf("and is 0\n");
+		printf(" and is 0\n");
 	}
 	if (lastdigi < 6 && lastdigi != 0)
 	{
-		printf("and is less than 6 and not 0");
+		printf(" and is less than 6 and not 0");
 	}
 	return (0);
 }
