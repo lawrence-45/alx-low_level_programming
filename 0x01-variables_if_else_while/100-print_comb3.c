77acd6ff5cfbb0266d7fea1184620d563819ca8e
lawrence-45 <lawrenceanekwe@gmail.com>
2022-10-14 20:49:52 -0700

    Answer Advanced task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 < 9; digit1++)
+	{
+		putchar((digit1 % 10) + '0');
+		putchar((digit2 % 10) + '0');
+
+		if (digit1 == 8 && digit2 == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+}
