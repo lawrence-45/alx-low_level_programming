b755ac667a6d0f2d8db0ecb12ff9bebaeb963d7f
lawrence-45 <lawrenceanekwe@gmail.com>
2022-10-19 06:20:25 -0700

    first commit

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char myletter);
+
+void print_alphabet(void);
+
+void Print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+void jack_bauer(void);
+
+void time_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+#endif
