75e28e7da3b5953ca4dc1b67e0e8157fa4d15550
lawrence-45 <lawrenceanekwe@gmail.com>
2022-10-20 17:21:24 -0700

    Modify last commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -14,10 +14,10 @@ void print_line(int n)
 
 	for (i = 0; i < n; i++)
 	{
-		putchar('');
+		_putchar(' ');
 		if (n <= 0)
 		{
-			_putchar('\n');
+			_putchar('_');
 		}
 	}
 	_putchar('\n');
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,7 +17,7 @@ int main(void)
 	for (a = 1; a <= 100; a++)
 	{
 		if (a % 15 == 0)
-		printf("FizzBuzz")
+			printf("FizzBuzz");
 		else if (a % 3 == 0)
 			printf("Fizz");
 		else if (a % 5 == 0)
